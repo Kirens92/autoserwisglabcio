642f6fc6bc1db471f11fa6da15935271054a379c
Kirens <kirens1043@gmail.com>
2026-08-07 02:16:00 +0200

    Remove incompatible prerender plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import vitePrerender from "vite-plugin-prerender";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,20 +19,13 @@ export default defineConfig({
       },
     },
   },
+
   build: {
     target: "es2019",
   },
-  plugins: [
-    react(),
-    vitePrerender({
-      staticDir: path.resolve(__dirname, "dist"),
-      routes: ["/"],
-      renderer: new (vitePrerender as any).PuppeteerRenderer({
-        headless: true,
-        args: ["--no-sandbox", "--disable-gpu"],
-      }),
-    }),
-  ],
+
+  plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
