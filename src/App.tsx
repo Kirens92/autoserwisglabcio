4546a317d82443aed06e3fd20dd3e42126e168aa
Kirens92 <kirens1043@gmail.com>
2026-09-08 23:09:39 +0200

    feat: add legal pages and payment footer

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import AdminSiteContent from "./pages/AdminSiteContent";
 import ServicesPage from "./pages/ServicesPage";
 import NewHomepage from "./pages/NewHomepage";
 import EcuTcuPage from "./pages/EcuTcuPage";
+import { PrivacyPolicyPage, TermsPage } from "./pages/LegalPages";
+import SiteLegalFooter from "./components/SiteLegalFooter";
 
 const queryClient = new QueryClient();
 
@@ -30,6 +32,8 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/nowa-strona" element={<NewHomepage />} />
           <Route path="/ecu-tcu" element={<EcuTcuPage />} />
+          <Route path="/regulamin" element={<TermsPage />} />
+          <Route path="/polityka-prywatnosci" element={<PrivacyPolicyPage />} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/admin/strona" element={<AdminSiteContent />} />
           <Route path="/admin/ecu-tcu" element={<AdminEcuTcu />} />
@@ -44,6 +48,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <SiteLegalFooter />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
