99362a417db8073d8400db0345309d30e6eb5afb
Kirens92 <kirens1043@gmail.com>
2026-09-08 19:23:02 +0200

    feat(routes): add ECU TCU landing page and CMS dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Admin from "./pages/Admin";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminEcuTcu from "./pages/AdminEcuTcu";
 import Realizations from "./pages/Realizations";
 import RealizationDetail from "./pages/RealizationDetail";
 import AdminRealizations from "./pages/AdminRealizations";
 import AdminSiteContent from "./pages/AdminSiteContent";
 import ServicesPage from "./pages/ServicesPage";
 import NewHomepage from "./pages/NewHomepage";
+import EcuTcuPage from "./pages/EcuTcuPage";
 
 const queryClient = new QueryClient();
 
@@ -24,8 +27,11 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/nowa-strona" element={<NewHomepage />} />
-          <Route path="/admin" element={<Admin />} />
+          <Route path="/ecu-tcu" element={<EcuTcuPage />} />
+          <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/admin/strona" element={<AdminSiteContent />} />
+          <Route path="/admin/ecu-tcu" element={<AdminEcuTcu />} />
+          <Route path="/admin/uslugi" element={<Admin />} />
           <Route path="/admin/realizacje" element={<AdminRealizations />} />
           <Route path="/realizacje" element={<Realizations />} />
           <Route path="/realizacje/:slug" element={<RealizationDetail />} />
