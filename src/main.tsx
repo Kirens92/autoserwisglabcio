3c9c7f6f57e8bd3a4b2272f4d1184a7b4f44d548
Kirens92 <kirens1043@gmail.com>
2026-09-08 21:43:15 +0200

    fix(brand): use clean 2026 Glabcio logo asset

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import glabcioLogo from "./lib/glabcioLogo";
+import glabcioLogo from "./assets/glabcio-logo-2026.svg";
 import "./index.css";
 
 const applyBrandLogo = () => {
