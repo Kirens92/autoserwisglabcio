13f911c3896b88d8a8795eb995447ee694900735
Kirens92 <kirens1043@gmail.com>
2026-09-09 18:35:58 +0200

    style: load booking and ECU responsive overrides

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,10 @@ import "./pages/NewHomepageBackgroundFix.css";
 import "./pages/NewHomepageReadabilityFix.css";
 import "./pages/NewHomepagePhoneOnly.css";
 import "./pages/NewHomepageBooking.css";
+import "./pages/ServicesBooking.css";
+import "./pages/EcuTcuPolish.css";
 import "./pages/EcuTcuLogoFix.css";
+import "./pages/EcuTcuResponsive.css";
 import "./lib/motowarsztatBooking";
 
 const applyBrandLogo = () => {
